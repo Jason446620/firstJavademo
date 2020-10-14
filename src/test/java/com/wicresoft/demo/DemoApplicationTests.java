676bd8d87edd732cca533786cd20b864efd552db
Jason446620 <panshubei@outlook.com>
2020-10-14 14:54:48 +0800

    init project

---
+++ src/test/java/com/wicresoft/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wicresoft.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
